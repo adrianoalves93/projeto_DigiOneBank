cf192a9f5346c7705cebc5d9cc12217514f18f0b
adrianoalves93 <adriano.alvescto@gmail.com>
2022-07-15 14:48:03 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,16 +4,12 @@ class Pessoa {
     var nome:String = "Jether"
     var cpf:String = "123.123.123-11"
     private set
-
+    constructor()
+    fun pessoaInfo() = "$nome e $cpf"
 
 }
 fun main (){
     val jether = Pessoa()
-
-    println(jether)
-    println(jether.nome)
-    println(jether.cpf)
-
-
+    println(jether.pessoaInfo())
 
 }
